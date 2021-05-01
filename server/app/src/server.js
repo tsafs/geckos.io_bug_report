ffd4cce2a1213717f55457d56c6cde699b93d325
Sebastian Fast <sfast92@gmail.com>
2021-05-01 09:30:34 +0200

    fixed cors

--- server/app/src/server.js
+++ server/app/src/server.js
@@ -2,9 +2,12 @@ const geckos = require('@geckos.io/server').default;
 const { iceServers } = require('@geckos.io/server');
 const http = require('http');
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
+
+app.use(cors());
 
 const io = geckos({
   iceServers: process.env.NODE_ENV === 'production' ? iceServers : [],
